26deb8093aeb718938081bf26fd9aed43d8a2c62
Daniel Lasota <slasher.root@gmail.com>
2025-04-26 10:47:17 +0200

    refactor: refactor VariablesCounter to MarketState

--- include/OrderBookSessionSimulator.h
+++ include/OrderBookSessionSimulator.h
@@ -1,10 +1,12 @@
 #ifndef ORDERBOOK_SESSION_SIMULATOR_H
 #define ORDERBOOK_SESSION_SIMULATOR_H
 
-#include <OrderBook.h>
+#include "OrderBookMetrics.h"
+#include "OrderBook.h"
+#include "FinalOrderBookSnapshot.h"
+
 #include <string>
 #include <pybind11/pybind11.h>
-#include "FinalOrderBookSnapshot.h"
 
 namespace py = pybind11;
 
@@ -12,9 +14,9 @@ class OrderbookSessionSimulator {
 public:
     OrderbookSessionSimulator();
 
-    void computeBacktest(const std::string& csvPath, const py::object &python_callback = py::none());
+    std::vector<OrderBookMetrics> computeVariables(const std::string &csvPath, std::vector<std::string> &variables);
 
-    void computeVariables(const std::string &csvPath, std::vector<std::string> &variables);
+    void computeBacktest(const std::string& csvPath, const py::object &python_callback = py::none());
 
     FinalOrderBookSnapshot computeFinalDepthSnapshot(const std::string &csvPath);
 

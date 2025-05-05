64ca7b82d3a0f12a130937b90afa4d8a78117522
Daniel Lasota <slasher.root@gmail.com>
2025-05-05 11:44:20 +0200

    feat: Fastest quant cup orderbook algorithm ever achieved

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main() {
     OrderBookSessionSimulator orderBookSessionSimulator;
 
     std::string csvPath = "C:/Users/daniel/Documents/merged_csvs/merged_depth_snapshot_difference_depth_stream_trade_stream_usd_m_futures_trxusdt_14-04-2025.csv";
-    // std::string csvPath = "python_tests/csv/test_positive_binance_depth_snapshot_difference_depth_stream_trade_stream_usd_m_futures_trxusdt_24-04-2025.csv";
+
     std::vector<std::string> variables {
         "timestampOfReceive",
         "bestAsk",

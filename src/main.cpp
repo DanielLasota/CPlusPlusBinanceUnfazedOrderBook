bce2ebc5e3e08fe253aa4d72e9e472cb679df648
Daniel Lasota <slasher.root@gmail.com>
2025-05-11 09:43:34 +0200

    feat: Update DifferenceDepthEntry to store all fields

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 // src/main.cpp
 #include "../include/OrderBook.h"
 #include "../include/OrderBookSessionSimulator.h"
+#include <iostream>
+
 
 void mainOrderbookEntryLoop(OrderBook &orderbook);
 
@@ -8,10 +10,7 @@ void processOrderbook();
 
 int main() {
     OrderBookSessionSimulator orderBookSessionSimulator;
-
-    std::string csvPath = "C:/Users/daniel/Documents/merged_csvs/merged_depth_snapshot_difference_depth_stream_trade_stream_usd_m_futures_trxusdt_14-04-2025.csv";
-//    std::string csvPath = "C:/Users/daniel/Documents/binance_archival_data/binance_difference_depth_stream_usd_m_futures_trxusdt_14-04-2025.csv";
-
+    std::string csvPath = "C:/Users/daniel/Documents/merged_csvs/binance_depth_snapshot_difference_depth_stream_trade_stream_usd_m_futures_trxusdt_14-04-2025.csv";
     std::vector<std::string> variables {
         "timestampOfReceive",
         "bestAsk",
@@ -20,7 +19,29 @@ int main() {
         "bestVolumeImbalance",
         "gap"
     };
-
     orderBookSessionSimulator.computeVariables(csvPath, variables);
+
+    // OrderBookSessionSimulator orderBookSessionSimulator;
+    // std::string csvPath = "C:/Users/daniel/Documents/binance_archival_data/binance_difference_depth_stream_usd_m_futures_trxusdt_14-04-2025.csv";
+    // OrderBook orderbook = orderBookSessionSimulator.computeFinalDepthSnapshot(csvPath);
+    // orderbook.printOrderBook();
+    //
+    // auto e = orderbook.getAsks()[0];
+    // std::cout
+    //     << e.TimestampOfReceive << " "
+    //     << e.Stream << " "
+    //     << e.EventType << " "
+    //     << e.EventTime << " "
+    //     << e.TransactionTime << " "
+    //     << e.Symbol << " "
+    //     << e.FirstUpdateId << " "
+    //     << e.FinalUpdateId << " "
+    //     << e.FinalUpdateIdInLastStream << " "
+    //     << e.IsAsk << " "
+    //     << e.Price << " "
+    //     << e.Quantity << " "
+    //     << e.PSUnknownField << " "
+    //     << "\n";
+
     return 0;
 }
